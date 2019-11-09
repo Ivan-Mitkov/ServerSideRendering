021e5da26c31855d4835bd811bcabf87bdbf0b78
unknown <ivan.mitkov@gmail.com>
2019-11-09 13:25:59 +0200

    init commit

---
+++ src/client/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home=(props)=>{
+return(
+    <div>
+        I'm the Home component
+    </div>
+)
+}
+
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express=require('express');
+const app=express()
+
+
+app.get('/',(res,req)=>{
+    
+})
+
+app.listen(3000,()=>console.log('app listen on port 3000'))
